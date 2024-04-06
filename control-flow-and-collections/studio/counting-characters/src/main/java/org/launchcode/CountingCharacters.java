be2654e2c54a1a41a9241379ebecf31df717c5c4
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-04-06 13:43:55 -0500

    Excercise | Completed

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
@@ -1,7 +1,9 @@
 package org.launchcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
 
@@ -25,3 +27,74 @@ public class CountingCharacters {
     }
 }
 }
+
+// ArrayListPractice
+//package org.launchcode;
+//
+//import java.util.ArrayList;
+//import java.util.Scanner;
+//
+//public class ArrayListPractice {
+//    public static void main(String[] args) {
+//
+//
+//        // Create an ArrayList with at least 10 integers
+//        ArrayList<Integer> numbers = new ArrayList<>();
+//        numbers.add(3);
+//        numbers.add(5);
+//        numbers.add(8);
+//        numbers.add(12);
+//        numbers.add(15);
+//        numbers.add(17);
+//        numbers.add(20);
+//        numbers.add(23);
+//        numbers.add(25);
+//        numbers.add(30);
+//
+//        // Call the method to find the sum of even numbers
+//        int evenSum = sumEven(numbers);
+//        System.out.println("Sum of even numbers: " + evenSum);
+//        System.out.println("-----------------------------------------------------------------------------------------");
+//
+//        ArrayList<String> fruits = new ArrayList<>();
+//        fruits.add("apple");
+//        fruits.add("banana");
+//        fruits.add("orange");
+//        fruits.add("grape");
+//        fruits.add("lemon");
+//        fruits.add("kiwi");
+//        fruits.add("melon");
+//
+//        // Call the method to print words with exactly 5 letters
+//        System.out.println("Words with exactly the number letters given by user:");
+//        fiveLetterWords(fruits);
+//    }
+//
+//    public static int sumEven(ArrayList<Integer> arr) {
+//
+//        int total = 0;
+//        for (int integer : arr) {
+//            if (integer % 2 == 0) {
+//                total += integer;
+//            }
+//        }
+//        return total;
+//    }
+//
+//    public static void fiveLetterWords (ArrayList <String> words){
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a word length: ");
+//        int numChars = input.nextInt();
+//
+//        for (String word : words) {
+//
+//            if(word.length() == numChars ){
+//                System.out.println(word);
+//            }
+//
+//        }
+//
+//    }
+//}
+
