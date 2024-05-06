de949bd7334ba974a8cc7d6403338ebfd5e27456
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-05-06 12:42:46 -0500

    Studio | Restaurant Menu | Completed | Bonus

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,31 +1,65 @@
 package org.launchcode;
+import com.sun.tools.javac.Main;
+
+import javax.sound.midi.VoiceStatus;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
     public class Menu {
+
+        private ArrayList<MenuItem> menuItems = new ArrayList<>();
         private LocalDate lastUpdated;
-        ArrayList<MenuItem> items = new ArrayList<>(); // Keeping it default
 
+        // Allow for default constructor
 
-            public Menu(LocalDate d, ArrayList<MenuItem> i) {
-                this.lastUpdated = d;
-                this.items = i;
-            }
-
-            public void setLastUpdated(LocalDate lastUpdated) {
-                this.lastUpdated = lastUpdated;
-            }
-
-            public void setItems(ArrayList<MenuItem> items) {
-                this.items = items;
-            }
-
-            public LocalDate getLastUpdated() {
-                return lastUpdated;
-            }
-
-            public ArrayList<MenuItem> getItems() {
-                return items;
-            }
-
+        public ArrayList<MenuItem> getMenuItems() {
+            return menuItems;
         }
+
+        @Override
+        public String toString() {
+            StringBuilder appetizers = new StringBuilder();
+            for (MenuItem item: menuItems){
+                if (item.getCategory().equals("appetizer")){
+                    appetizers.append("\n").append(item.toString()).append("\n");
+                }
+            }StringBuilder mainCourses = new StringBuilder();
+            for (MenuItem item: menuItems){
+                if (item.getCategory().equals("main course")){
+                    mainCourses.append("\n").append(item.toString()).append("\n");
+                }
+            }StringBuilder desserts = new StringBuilder();
+            for (MenuItem item: menuItems){
+                if (item.getCategory().equals("dessert")){
+                    desserts.append("\n").append(item.toString()).append("\n");
+                }
+            }
+            return "\nEMILY'S PIZZA MENU\n" +
+                    "APPETIZERS" + appetizers.toString() + "\n" +
+                    "MAIN COURSES" + mainCourses.toString() + "\n" +
+                    "DESSERTS" + desserts.toString() + "\n";
+        }
+
+        void addItem(MenuItem newItem){
+            String message = "That item has already been added to the menu.";
+
+            if (menuItems.contains(newItem)) {
+                System.out.println(message);
+                return;
+            }
+
+            for (MenuItem item : menuItems) {
+                if (item.equals(newItem)) {
+                    System.out.println(message);
+                    return;
+                }
+            }
+            menuItems.add(newItem);
+            lastUpdated = LocalDate.now();
+        }
+
+        void removeItem(MenuItem item){
+            menuItems.remove(item);
+            lastUpdated = LocalDate.now();
+        }
+    }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,32 +1,87 @@
 package org.launchcode;
 
+import com.sun.source.tree.ReturnTree;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
 public class MenuItem {
 
-    private double price;
+    private String name;
     private String description;
+    private double price;
     private String category;
-    private boolean isNew;
+    private final LocalDate dateAdded;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
-        this.price = p;
-        this.description = d;
-        this.category = c;
-        this.isNew = iN;
+    public MenuItem(String name , String description, double price, String category ) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.dateAdded = LocalDate.now(); // when we instantiate something get the date write this moment and it final so can't be changed
+        // this.dateAdded = LocalDate.parse("2022-06-22"); // Use line below instead of line above to test isNew() returning false
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
     public void setCategory(String category) {
         this.category = category;
     }
 
-    public void setNew(boolean aNew) {
-        isNew = aNew;
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    @Override
+    public String toString() {
+        String newText = isNew() ? "- New!" : "";
+        return name + newText + "\n" + description + " | $" + price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuItem menuItem = (MenuItem) o;
+//        return Objects.equals(name, menuItem.name) && Objects.equals(description, menuItem.description) && Objects.equals(category, menuItem.category) && Objects.equals(dateAdded, menuItem.dateAdded);
+        return this.name.equals(menuItem.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, price, category, dateAdded);
+    }
+
+    boolean isNew() {
+        LocalDate today = LocalDate.now();
+        double daysBetween = getDateAdded().until(today, ChronoUnit.DAYS);
+        return daysBetween < 90 ;
     }
 }
