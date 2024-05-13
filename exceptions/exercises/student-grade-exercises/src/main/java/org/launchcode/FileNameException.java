9ff47eb8a24a847f6cb474e1584fb55e3f862233
aakash-sachdev <aakashsachdev1992@gmail.com>
2024-05-13 18:42:18 -0500

    Exceptions | Completed

---
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/FileNameException.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+public class FileNameException extends Exception{
+
+    public FileNameException(String message){
+        super(message);
+    }
+
+}
--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,15 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        int a = 10;
+        int b = 0;
+
+        Divide(a,b);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,16 +18,43 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for(Map.Entry<String, String> student : studentFiles.entrySet())
+            System.out.println(student.getKey() + " : " + CheckFileExtension(student.getValue()));
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+        try {
+            int grade = x/y;
+            System.out.println("The student's grade is " + grade);
+        } catch (ArithmeticException e) {
+            System.out.println("You cannot divide by zero!");
+            e.printStackTrace();
+        }
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
-        return 0;
+        try {
+            if (fileName.isEmpty() || fileName == null) {
+                throw new FileNameException("The file name is empty or null.");
+            }
+            int endIndex = fileName.length();
+            int startIndex = fileName.length() - 5;
+            String sub = fileName.substring(startIndex, endIndex);
+
+            if(sub.equals(".java")) {
+//                System.out.println(sub);
+                return 1;
+            }
+            else
+            {
+//                System.out.println(sub);
+                return 0;
+            }
+        } catch (FileNameException e){
+            e.printStackTrace();
+            return -1;
+        }
     }
 }
